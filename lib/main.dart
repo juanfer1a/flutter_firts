d1fd694f41cfab3a35813d5794970fe9821d3b58
juanfer1a <jundi1a@gmail.com>
2022-11-22 11:14:36 -0500

    Final part 1

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,10 @@ class MyApp extends StatelessWidget {
     //juan prueba
     //final wordPair = WordPair.random(); // Add this line.
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Startup Name Generator'),
         ),
         body: const Center(
           //child: Text('Hello World'),
@@ -35,9 +35,26 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[]; // NEW
+  final _biggerFont = const TextStyle(fontSize: 18); // NEW
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random(); // NEW
-    return Text(wordPair.asPascalCase); // NEW
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
+    );
   }
 }
